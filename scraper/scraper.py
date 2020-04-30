6f2238645b2f74fc0dfdc7a9da09aa2c7732977d
Colin Page <cwpage@umich.edu>
2020-04-30 01:02:44 -0400

    Finished Script for grabbing beatles lyrics

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,6 +6,7 @@ from rauth import OAuth1Service
 import requests
 import time
 from secret import ACCESS_TOKEN
+import re
 
 
 class Genius_API():
@@ -41,6 +42,31 @@ class Genius_API():
 
         return response.json()['response']
 
+    def get_song_lyrics(self, url):
+        song_page = requests.get(url)
+
+        # Make sure the page still exists
+        if song_page.status_code == 404:
+            return None
+
+        page_html = bs(song_page.text, "html.parser")
+
+        lyric_section = page_html.find('div', class_="lyrics")
+        if not lyric_section:
+            return None
+
+        lyrics = self.clean_lyrics(lyric_section.get_text())
+
+        time.sleep(self.sleep_time)
+
+        return lyrics
+
+    def clean_lyrics(self, lyrics):
+        lyrics = re.sub('(\[.*?\])*', '', lyrics)
+        lyrics.strip()
+
+        return lyrics
+
 
 def scrape():
     genius = Genius_API()
@@ -50,24 +76,27 @@ def scrape():
     while page:
         response = genius.get_beatles_songs(page=page)
         for song in response['songs']:
-            songs.append([clean_title(song['title']), song['id']])
+            songs.append([clean_title(song['title']), song['url']])
 
         page = response['next_page']
 
     for song in songs:
         # get song_lyrics
-        lyrics = genius.get_song_lyrics(id=song[1])
+        lyrics = genius.get_song_lyrics(url=song[1])
         # save to .txt file
-        lyric_file = open("data/{}.txt".format(song[0]), "w")
-        lyric_file.write(lyrics)
-        lyric_file.close()
+        if lyrics:
+            lyric_file = open("../data/{}.txt".format(song[0]), "w")
+            lyric_file.write(lyrics)
+            lyric_file.close()
 
 
 def clean_title(title):
     title = title.replace(' ', '_')
-    title = title.replace("’", '')
     title = title.replace('[', '')
     title = title.replace(']', '')
+    title = title.replace('(', '')
+    title = title.replace(')', '')
+    title = title.replace('’', '')
 
     return title
 
