815e4ca0dcc7dfdfaee730cf754630e73954fbc8
Colin Page <cwpage@umich.edu>
2020-04-29 22:55:22 -0400

    Added setup.py and .gitignore

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""Package Configuration for AIbbeyRoad."""
+
+from setuptools import setup
+
+setup(
+    name='ai_road',
+    version='0.0.0',
+    packages=['ai_road'],
+    include_package_data=True,
+    install_requires=[
+        'numpy==1.18.1',
+        'matplotlib',
+        'torch',
+        'tqdm',
+        'bs4',
+        'pandas'
+    ],
+)
