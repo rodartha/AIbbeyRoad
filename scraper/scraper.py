3b8479b79ca4d9ff73cedd03af8cc8b31f81fbd1
Colin Page <cwpage@umich.edu>
2020-04-30 00:23:59 -0400

    progress on accessing songs

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -27,13 +27,13 @@ class Genius_API():
         # Genius artist id for the beatles
         self.BEATLES_ID = 586
 
-    def get_beatles_songs(self):
+    def get_beatles_songs(self, page=1):
         uri = self.root + "artists/{}/songs".format(self.BEATLES_ID)
         params = {
             'text_format': 'plain',
             'sort': 'title',
             'per_page': 30,
-            'page': 1
+            'page': page
         }
 
         response  = self.session.request('GET', uri, timeout=self.timeout, params=params)
@@ -45,7 +45,33 @@ class Genius_API():
 def scrape():
     genius = Genius_API()
 
+    """
     response = genius.get_beatles_songs()
+
+    #print(response['songs'][0])
+
+    print(response['next_page'])
+    """
+    songs = []
+    page = 1
+    while page:
+        response = genius.get_beatles_songs(page=page)
+        for song in response['songs']:
+            title = song['title'].replace(' ', '_')
+            title = title.replace("’", '')
+            title = title.replace('[', '')
+            title = title.replace(']', '')
+            songs.append([title, song['id']])
+
+        page = response['next_page']
+
+    for song in songs:
+        # get song_lyrics
+        lyrics = genius.get_song_lyrics(id=song[1])
+        # save to .txt file
+        lyric_file = open("data/{}.txt".format(song[0]), "w")
+        lyric_file.write(lyrics)
+        lyric_file.close()
 
 
 scrape()
